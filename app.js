e5a068443d31b824808708c19318f85b115623ac
Prithwi Kumar <prithwikumar871@gmail.com>
2024-12-28 21:35:16 +0530

    PrintName fn added

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const age = 21;
-const name = "prithwi singh ";
+const firstName = "prithwi singh ";
 const dob = '10/03/2004';
 
 function sayName(){
-    console.log('My name is ' + name);
+    console.log('My name is ' + firstName);
 }
 
 sayName();
+
+function printName(){
+    for(let i=0; i<5; i++){
+        console.log(firstName);
+    }
+}
+printName();
