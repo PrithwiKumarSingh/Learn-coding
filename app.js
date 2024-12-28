95e56c10415e3de87d53bd06de52d72d5ece63a8
Prithwi Kumar <prithwikumar871@gmail.com>
2024-12-28 21:04:20 +0530

    initial added

---
+++ app.js
@@ -0,0 +1,9 @@
+const age = 21;
+const name = "prithwi singh ";
+const dob = '10/03/2004';
+
+function sayName(){
+    console.log('My name is ' + name);
+}
+
+sayName();
---
+++ utils/utils.js
@@ -0,0 +1,6 @@
+function greeting(){
+    for(let i=0; i<5; i++){
+        console.log("Hello Guys. What'sUp !");
+    }
+}
+greeting();
