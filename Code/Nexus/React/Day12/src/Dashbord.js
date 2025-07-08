6d16c96d8de38ff50541824858c6da2613947ab1
Prithwi Kumar <prithwikumar871@gmail.com>
2025-07-08 22:29:23 +0530

    React-Router

---
+++ Code/Nexus/React/Day12/src/Dashbord.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router"
+export default function Dashbord(){
+
+    return(
+    <>
+        <nav>
+            <Link to="/" >Home</Link>
+            <Link to="/Dashbord" >Dashbord</Link>
+            <Link to="/Contact" >Contact</Link>
+            <Link to="/About" >About</Link>
+            <Link to="/Details">Details</Link>
+        </nav>
+        <h1>I am Dashbord page </h1>
+        </>
+    )
+}
