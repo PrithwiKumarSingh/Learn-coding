46c11b527cf48816217d2907769e032d3fe4b998
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-07 00:28:17 +0530

    Variable & Datatypes

---
+++ Code/JavaScript/script.js
@@ -0,0 +1,13 @@
+// let age = 21;
+// const name = "Prithwi";
+// var city = "Varanasi";
+
+// console.log(`age ${age}, name ${name}, city ${city}`);
+
+
+let num = 42;         // number
+let text = "hello";   // string
+let isCool = true;    // boolean
+let empty = null;     // null
+let notSet;           // undefined
+console.log(typeof notset);
