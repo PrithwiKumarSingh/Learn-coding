0fbff8a617f50b79185c8f0eb196705bc2b8b545
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-07 11:31:00 +0530

    Assignment- if/else

--- Code/JavaScript/script.js
+++ Code/JavaScript/script.js
@@ -5,9 +5,53 @@
 // console.log(`age ${age}, name ${name}, city ${city}`);
 
 
-let num = 42;         // number
-let text = "hello";   // string
-let isCool = true;    // boolean
-let empty = null;     // null
-let notSet;           // undefined
-console.log(typeof notset);
+// let num = 42;         // number
+// let text = "hello";   // string
+// let isCool = true;    // boolean
+// let empty = null;     // null
+// let notSet;           // undefined
+// console.log(typeof notset);
+
+
+// let arr = [1,2,4,-5,6,7,"Prithwi", true];
+// console.log(arr[7]);
+
+// let num = arr[3];
+
+// if(num>0)console.log("Positive")
+//     else console.log("Negative")
+
+
+// let users = ["harkirat", "raman", "Prithwi", "Kajal", "Aman"];
+
+// for(let i=0; i<users.length;i++){
+//     console.log(users[i]);
+// } 
+
+// Assignment 
+
+// let a = 10;
+// let sum = 0;
+// for(let i=1; i<=a; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+// const sum = (a,b)=>{return console.log(a + b)};
+// sum(5,4);
+// sum("Prithwi"," Singh");
+
+
+// Assignment #2
+// Write a function called canVote that returns true or false if the age of a user is > 18
+
+// age = 15;
+
+
+
+// if(age>=18){
+//     console.log("canVote");
+// }else{
+//     console.log("You are minor can'tVote");
+// }
+
