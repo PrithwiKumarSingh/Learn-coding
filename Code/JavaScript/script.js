52756a89b1314f16fceba031be04c3edecdfadd4
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-07 12:13:44 +0530

    little bit Complex Assignment

--- Code/JavaScript/script.js
+++ Code/JavaScript/script.js
@@ -55,3 +55,61 @@
 //     console.log("You are minor can'tVote");
 // }
 
+// let num = 30;
+
+// if(num%2==0){
+//     console.log("The Number is even");
+// }else{ console.log("The number is odd")};
+
+
+// Object 
+
+// let user = {
+//     name : "Prithwi",
+//     age : 21
+// }
+// console.log("Prithwi age is "+user.age);
+
+// Assignment #1
+// Write a function that takes a user as an input and greets them with their name and age
+
+// let data = {
+//     name : "Prithwi",
+//     age : 34,
+
+// }
+// const user = (data)=>{return console.log(" Hello "+data.name +"Your age is : "+data.age)};
+// user(data);
+
+let user1 = {
+    firstName: "Mr.",
+    middleName : "Prithwi",
+    lastName : "Kumar",
+    age : 21,
+    gender : "Male"
+}
+let user2 = {
+    firstName: "Mrs.",
+    middleName : "Anamika",
+    lastName : "Singh",
+    age : 22,
+    gender : "Female"
+}
+
+const vote = (data)=>{
+    if(data>=18){
+        return " You Can Vote. ";
+    }else{ return "You Can't Vote (underage)"}
+}
+
+const show = (data)=>{
+        let res = vote(data.age);
+    console.log(`Hello, ${data.firstName} ${data.middleName} ${data.lastName} 
+                 Your age is ${data.age} & 
+                 Your gender is ${data.gender}.
+                 ${res}`)
+    }
+show(user1);
+show(user2);
+
+
