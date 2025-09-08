c9312ecff97b738124dd7f972ab5f775c5d9bba6
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-08 20:23:02 +0530

    Passing Function as Arguments

--- Cohort 3.0/Week-2/script.js
+++ Cohort 3.0/Week-2/script.js
@@ -1,21 +1,60 @@
-//ParseInt parseFloat 
+// //ParseInt parseFloat 
+// function sum(a,b){
+//     return parseInt(a) + parseFloat(b);
+// }
+
+// let ans = sum('2.2',"40.8");
+// console.log(ans);
+
+
+// //   find sum from 1 to a number 
+
+// function sum(n){
+//     let sum = 0; 
+//     for(let i=1; i<=n; i++){
+//         sum += i;
+//     }
+//     return sum;
+// }
+
+// let res = sum(100);
+// console.log(res);
+
+
+// function sumFormula(num){
+//     // let sum = ((num+1)*num) /2;
+//     let sum = (num*(num+1)) /2;
+//     return sum;
+
+// }
+
+// console.log(sumFormula(100));
+
+
+
+// const fs = require("fs");
+// const contents = fs.readFileSync("a.txt", "utf-8");
+// console.log(contents);
+// const content = fs.readFileSync("b.txt", "utf-8");
+// console.log(content);
+
+
 function sum(a,b){
-    return parseInt(a) + parseFloat(b);
+    return a+b;
+}
+function Subtraction(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
 }
 
-let ans = sum('2.2',"40.8");
-console.log(ans);
-
-
-//   find sum from 1 to a number 
-
-function sum(n){
-    let sum = 0; 
-    for(let i=1; i<=n; i++){
-        sum += i;
-    }
-    return sum;
+function doOperation(a,b,op){
+    return op(a,b);
 }
 
-let res = sum(100);
+let res = doOperation(4,2,multiply);
 console.log(res);
