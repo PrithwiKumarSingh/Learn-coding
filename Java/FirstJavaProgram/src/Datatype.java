b95f24ffef1c865534ab485ec4a9d6ae87f0530e
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-12 21:25:30 +0530

    escape sequence \n

---
+++ Java/FirstJavaProgram/src/Datatype.java
@@ -0,0 +1,23 @@
+public class Datatype {
+    public static void main(String[] args) {
+        // int num = 29;
+        // System.out.println(num);
+
+        // float n = 12.3f;
+        // System.out.println(n);
+
+        // escape sequences 
+
+        System.out.println("* \n* * \n* * * \n* * * * \n* * * * *");
+        System.out.println("");
+        System.out.println("* * * * * \n* * * * \n* * * \n* * \n*");
+        System.out.println("");
+        System.out.println("         *\n       * *\n     * * *\n   * * * *\n * * * * *");
+
+
+
+        
+        
+    }
+     
+}
