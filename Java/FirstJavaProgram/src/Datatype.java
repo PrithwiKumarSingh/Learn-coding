dd014fcfbd6ec5d231fcab955de011c625c5859f
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-12 21:43:16 +0530

    Scanner in java Uses

--- Java/FirstJavaProgram/src/Datatype.java
+++ Java/FirstJavaProgram/src/Datatype.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Datatype {
     public static void main(String[] args) {
         // int num = 29;
@@ -8,13 +9,36 @@ public class Datatype {
 
         // escape sequences 
 
-        System.out.println("* \n* * \n* * * \n* * * * \n* * * * *");
-        System.out.println("");
-        System.out.println("* * * * * \n* * * * \n* * * \n* * \n*");
-        System.out.println("");
-        System.out.println("         *\n       * *\n     * * *\n   * * * *\n * * * * *");
+        // System.out.println("* \n* * \n* * * \n* * * * \n* * * * *");
+        // System.out.println("");
+        // System.out.println("* * * * * \n* * * * \n* * * \n* * \n*");
+        // System.out.println("");
+        // System.out.println("         *\n       * *\n     * * *\n   * * * *\n * * * * *");
 
 
+        // 1. Create a program to input name of the person and respond with "Welcome Name to KG Coding"
+
+        // Scanner input = new Scanner(System.in);
+
+        // System.out.print("Please Enter Your Name : ");
+        // String User = input.nextLine();
+
+        // System.out.println("Welcome "+ User + " to KG Coding ");
+
+
+
+        // 2. Create a Program to add two numbers. 
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please Enter Your First Number : ");
+        int num1 = input.nextInt();
+        System.out.print("Please Enter Your Second Number : ");
+        int num2 = input.nextInt();
+
+        int Sum = num1 + num2;
+        System.out.println("Sum of "+num1+ " + "+num2+ " is : "+Sum);
+        
 
         
         
