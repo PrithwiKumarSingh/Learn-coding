31797cb4c21e8d5bf433f07f0a8ddfe3bbdf80b8
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-20 23:09:04 +0530

    Vowel & Consonent

---
+++ CPP/LH03/second.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // char ch = 'K';
+
+    // if(ch == 'a' || ch == 'A')
+    // cout<<"Vowel"<<endl;
+    // else if(ch == 'e' || ch == 'E')
+    // cout<<"Vowel"<<endl;
+    // else if(ch == 'i' || ch == 'I')
+    // cout<<"Vowel"<<endl;
+    // else if(ch == 'o' || ch == 'O')
+    // cout<<"Vowel"<<endl;
+    // else if(ch == 'u' || ch == 'U')
+    // cout<<"Vowel"<<endl;
+    // else
+    // cout<<"Consonent"<<endl;
+
+
+
+    // int num = 15;
+
+    // if(num%3 == 0 && num%5 == 0){
+    //     cout<<" Divisible "<<endl;
+    // }else{
+    //     cout<<" Not Divisible "<<endl;
+    // }
+
+    
+
+    // int num = 73;
+
+
+    // if(num%7 == 0 || num%11 == 1)
+    // cout<<"Perfect"<<endl;
+    // else
+    // cout<<" Not Perfect "<<endl;
+
+
+
+
+
+
+
+
+
+    return 0;
+
+}
