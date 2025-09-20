7c9cb28dde4c444b575e304f009d5b7deaa950ab
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-20 11:50:29 +0530

    Lecture-2 C/D

--- CPP/first.cpp
+++ CPP/first.cpp
@@ -19,15 +19,38 @@ int main(){
 
     // find Odd or Even 
 
-    int num = 3;
-    if(num%2 == 0){
-        cout<<num<<" is Even."<<endl;
+    // int num = 3;
+    // if(num%2 == 0){
+    //     cout<<num<<" is Even."<<endl;
+    // }else{
+    //     cout<<num<<" is Odd."<<endl;
+    // }
+
+    // int num = 0;
+    
+    // if(num >0){
+    //     cout<<"Positive"<<endl;
+    // }else if(num<0){
+    //     cout<<"Negative"<<endl;
+    // }else{
+    //     cout<<"Zero"<<endl;
+    // }
+
+
+    // a,b,c which on is big 
+
+    int a = 1000, b = 200, c = 30;
+    
+    if(a > b && a > c){
+        cout<<a<<" is Biggeer . "<<endl;
+    }else if(b > c){
+        cout<<b<<" is Bigger . "<<endl;
     }else{
-        cout<<num<<" is Odd."<<endl;
+        cout<<c<<" is Bigger . "<<endl;
     }
 
-
-
+    float num = 3.544;
+    cout<<sizeof(num)<< endl;
 
 
     return 0;
