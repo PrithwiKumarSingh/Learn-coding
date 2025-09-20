59e7765f87e2e11da32b09f22e89e31af6babd78
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-20 10:39:22 +0530

    Odd & Even

--- CPP/first.cpp
+++ CPP/first.cpp
@@ -7,15 +7,24 @@ int main(){
 
     // Biggest Number 
     // int first_number = 200, second_number = 30;
-    int first_number = 20;
-    int second_number = 30;
+    // int first_number = 20;
+    // int second_number = 30;
 
-    if(first_number > second_number){
-        cout<<first_number<<endl;
+    // if(first_number > second_number){
+    //     cout<<first_number<<endl;
+    // }else{
+    //     cout<<second_number<<endl;
+    // }
+
+
+    // find Odd or Even 
+
+    int num = 3;
+    if(num%2 == 0){
+        cout<<num<<" is Even."<<endl;
     }else{
-        cout<<second_number<<endl;
+        cout<<num<<" is Odd."<<endl;
     }
-
 
 
 
