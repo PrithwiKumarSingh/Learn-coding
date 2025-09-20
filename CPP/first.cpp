a6e8f0ff4183151365fcef6464eca7cdf9b021bb
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-20 10:37:09 +0530

    CPP IF Else (Big Num)

---
+++ CPP/first.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // bool num = true;
+    // cout<<num<<endl;
+
+
+    // Biggest Number 
+    // int first_number = 200, second_number = 30;
+    int first_number = 20;
+    int second_number = 30;
+
+    if(first_number > second_number){
+        cout<<first_number<<endl;
+    }else{
+        cout<<second_number<<endl;
+    }
+
+
+
+
+
+
+    return 0;
+}
