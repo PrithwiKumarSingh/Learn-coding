d36eb9859f9f36016f1eb00cb9318b9fb5f5494a
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-22 21:11:16 +0530

    Prime Number Checker

--- CPP/LH04/first.cpp
+++ CPP/LH04/first.cpp
@@ -3,13 +3,37 @@ using namespace std;
 int main(){
 
     // Table Printing 
+    // int num ; 
+    // cout<<"Enter a Number : ";
+    // cin>>num;
+
+    // for(int i=1; i<=10; i++){
+    //     cout<<i*num<<" ";
+    // }
+    // cout<<endl;
+
+
     int num ; 
     cout<<"Enter a Number : ";
     cin>>num;
+    bool isPrime = true; 
 
-    for(int i=1; i<=10; i++){
-        cout<<i*num<<" ";
+    if(num < 2){
+        cout<<num<< " is not a Prime Number."<<endl;
+        return 0;
     }
-    cout<<endl;
+
+    for(int i=2; i<num-1; i++){
+        if(num%i == 0){
+            cout<<num<< " is not a Prime Number."<<endl;
+        }
+    }
+    cout<<num<< " is a Prime Number."<<endl;
+
+
+
+
+
+
     return 0;
 }
