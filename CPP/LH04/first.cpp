3a3da00a65e04e547b2e4268348c7a8a89dea00c
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-22 20:37:05 +0530

    Table printing

---
+++ CPP/LH04/first.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    // Table Printing 
+    int num ; 
+    cout<<"Enter a Number : ";
+    cin>>num;
+
+    for(int i=1; i<=10; i++){
+        cout<<i*num<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
