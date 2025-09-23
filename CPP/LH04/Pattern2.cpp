21f4470887d377f0d9da43cf70ad299f8fa8f2c0
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-23 10:52:13 +0530

    Pattern Formula ( n-col+-1 )

--- CPP/LH04/Pattern2.cpp
+++ CPP/LH04/Pattern2.cpp
@@ -1,20 +1,51 @@
-/*
+#include <iostream>
+using namespace std; 
+int main(){
+    /*
 1 2 3 4 5 
 1 2 3 4 5 
 1 2 3 4 5 
 1 2 3 4 5
 1 2 3 4 5
 */
+    // int n = 5; 
+    // for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=n; j++){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
 
 
-#include <iostream>
-using namespace std; 
-int main(){
+
+
+
+/*
+5 4 3 2 1 
+5 4 3 2 1 
+5 4 3 2 1 
+5 4 3 2 1 
+5 4 3 2 1 
+*/
+
+    //first Method to print this Pattern 
     int n = 5; 
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            cout<<j<<" ";
+    for(int row = 1; row<=n; row++){
+        for(int col=1; col<=n; col++){
+            cout<<n-col+1<<" ";
         }
         cout<<endl;
     }
+
+    //second method to print this Pattern 
+    // int n = 5;
+    // for(int row = 5; row>=1; row-- ){
+    //         for(int col = 5; col>=1; col--){
+    //             cout<<col<<" ";
+    //         }
+    //         cout<<endl;
+    // }
+
+
+    return 0; 
 }
