677148e45a080db2f11ee77aaac57866d2af15a1
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-23 11:08:07 +0530

    Pattern 3

---
+++ CPP/LH04/Pattern3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+/*
+
+1 2 3 4 5 
+6 7 8 9 10 
+11 12 13 14 15 
+16 17 18 19 20 
+21 22 23 24 25
+
+*/
+    int n = 5;
+    int counter = 1; 
+    for(int row=1; row<=n; row++){
+        for(int col=1; col<=n; col++){
+            cout<<counter<<" ";
+            counter += 1; 
+        }
+        cout<<endl;
+    }
+
+    return 0; 
+    
+}
