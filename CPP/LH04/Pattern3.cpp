9412d23f177625ab7242c3511090bf73cf4460fd
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-23 11:21:38 +0530

    Pattern 3 P4

--- CPP/LH04/Pattern3.cpp
+++ CPP/LH04/Pattern3.cpp
@@ -42,11 +42,41 @@ int main(){
 
  // Pattern - 3; 
 
-        int n = 5; 
+ /*
+
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+
+*/
+
+        // int n = 5; 
+        // for(int row=1; row<=n; row++){
+        //     for(int col=1; col<=row; col++){
+        //         cout<<col<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+
+
+    /*
+    
+a a a a a 
+b b b b b 
+c c c c c 
+d d d d d 
+e e e e e 
+
+*/
+        int n = 5;
+        char ch = 'a';
         for(int row=1; row<=n; row++){
-            for(int col=1; col<=row; col++){
-                cout<<col<<" ";
+            for(int col=1; col<=n; col++ ){
+                cout<<ch<<" ";
             }
+            ch++;
             cout<<endl;
         }
     
