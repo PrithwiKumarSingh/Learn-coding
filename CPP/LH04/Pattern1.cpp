2fdd5056589bb422f06e3b8f9db407ae5370f341
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-23 10:32:15 +0530

    Pattern Printing

---
+++ CPP/LH04/Pattern1.cpp
@@ -0,0 +1,23 @@
+/*
+1 1 1 1 1 
+1 1 1 1 1 
+1 1 1 1 1
+1 1 1 1 1
+1 1 1 1 1
+*/
+
+
+
+#include <iostream>
+using namespace std; 
+int main(){
+    int n = 5;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            cout<<" 1 ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
--- CPP/LH04/first.cpp
+++ CPP/LH04/first.cpp
@@ -2,16 +2,13 @@
 using namespace std;
 int main(){
 
-    // Table Printing 
-    // int num ; 
-    // cout<<"Enter a Number : ";
-    // cin>>num;
+    // int a = 2>1;   
+    // int b = 3>=2>1;   
+    // cout<<a<<endl; //   0  ---> True
+    // cout<<b<<endl; //   1  ---> False
 
-    // for(int i=1; i<=10; i++){
-    //     cout<<i*num<<" ";
-    // }
-    // cout<<endl;
-
+    int a = 382094 && 5492; 
+    cout<<a<<endl;
 
     
 }
