868aed711ac97ad1eb48467b6cb81b5a990fabff
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-25 00:41:34 +0530

    String reverse

--- Java/College/StringMethods.java
+++ Java/College/StringMethods.java
@@ -3,37 +3,46 @@
 public class StringMethods {
 
     public static void main(String[] args){
-        String name1 = "Prithwi Kumar";
-        String name2 = "Prithwi Kumar";
-        String name = new String("Prithwi Kumar");
-        System.out.println(name);
+        // String name1 = "Prithwi Kumar";
+        // String name2 = "Prithwi Kumar";
+        // String name = new String("Prithwi Kumar");
+        // System.out.println(name);
 
-        // .equals() --> compare variable inside content 
-        System.out.println(name1.equals(name2));
-        System.out.println(name2.equals(name));
+        // // .equals() --> compare variable inside content 
+        // System.out.println(name1.equals(name2));
+        // System.out.println(name2.equals(name));
 
-        // ==
-        System.out.println(name1 == name2);
-        System.out.println(name2 == name1);
-        System.out.println(name2 == name);
-        System.out.println(name1 == name);
+        // // ==
+        // System.out.println(name1 == name2);
+        // System.out.println(name2 == name1);
+        // System.out.println(name2 == name);
+        // System.out.println(name1 == name);
 
 
-        // length
-        System.out.println(name.length());
+        // // length
+        // System.out.println(name.length());
 
-        // .substring
-        System.out.println(name.substring(7));
-        System.out.println(name.substring(0, 7));
-        System.out.println(name.substring(8, 13));
+        // // .substring
+        // System.out.println(name.substring(7));
+        // System.out.println(name.substring(0, 7));
+        // System.out.println(name.substring(8, 13));
 
-        // .charAt()
-        // for(int i=0; i<name.length(); i++){
-        //     System.out.println(name.charAt(i));
-        // }
-        System.out.println(name.charAt(5));
+        // // .charAt()
+        // // for(int i=0; i<name.length(); i++){
+        // //     System.out.println(name.charAt(i));
+        // // }
+        // System.out.println(name.charAt(5));
 
-        //
+        //   Q1. String Reverse 
+            String nam = "Prithwi";
+
+            for(int i=nam.length()-1;i>=0 ; i--){
+                System.out.print(nam.charAt(i));
+            }
+            System.out.println();
+        
+    
+
 
     }
     
