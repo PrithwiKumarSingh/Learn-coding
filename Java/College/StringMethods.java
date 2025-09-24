7cc7cec25d44bdff4b560806cdda25453559ce8d
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-25 00:23:52 +0530

    String Method (length() & substring())

---
+++ Java/College/StringMethods.java
@@ -0,0 +1,19 @@
+
+
+public class StringMethods {
+
+    public static void main(String[] args){
+        String name = "Prithwi Kumar";
+        System.out.println(name);
+
+        // length
+        System.out.println(name.length());
+
+        // .substring
+        System.out.println(name.substring(7));
+        System.out.println(name.substring(0, 7));
+        System.out.println(name.substring(8, 13));
+
+    }
+    
+}
