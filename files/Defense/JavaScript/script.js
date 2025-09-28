7e5ea854c9863ea1f353768295947c47b246eef1
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-09-29 00:46:10 +0530

    Promise.All

--- files/Defense/JavaScript/script.js
+++ files/Defense/JavaScript/script.js
@@ -1,36 +1,59 @@
 
-let firstPromis = new Promise((resolve, reject)=>{
-    console.log("Adam Chal gya ! ")
-});
+// let firstPromis = new Promise((resolve, reject)=>{
+//     console.log("Adam Chal gya ! ")
+// });
 
 
-let Promise1 = new Promise((resolve, reject)=>{
-    let success = true; 
-    if(success){
-        resolve("Promise fullfiled");
-    }else{
-        reject("Promise Rejected");
+// let Promise1 = new Promise((resolve, reject)=>{
+//     let success = true; 
+//     if(success){
+//         resolve("Promise fullfiled");
+//     }else{
+//         reject("Promise Rejected");
+//     }
+// })
+
+// Promise1.then((massage)=>{
+//     console.log("The ka massage is "+ massage);
+// }).catch((error)=>{
+//     console.log("Error : "+error);
+// })
+
+
+// // promise chaining 
+// Promise1.then((massage)=>{
+//     console.log("first msg : "+massage);
+//     return ("Promise Fullfild ho gya Second msg");
+// }).then((massage)=>{
+//     console.log("Second msg : "+ massage);
+//     return ("second massage fullfiled");
+// }).then((massage)=>{
+//     console.log("Third msg : " +massage);
+// }).catch((error)=>{
+//     console.log("Error: " + error);
+// }).finally((massage)=>{
+//     console.log("Main to final hoon : chalunga hi chalunga");
+// })
+
+
+let Promise1 = new Promise((resolve,reject)=>{
+    setTimeout(resolve,2000,"First");
+})
+let Promise2 = new Promise((resolve,reject)=>{
+    setTimeout(resolve,2000,"second");
+})
+let Promise3 = new Promise((resolve,reject)=>{
+    setTimeout(resolve,2000,"third");
+})
+let Promise4 = new Promise((resolve,reject)=>{
+    setTimeout(resolve,2000,"forth");
+})
+
+Promise.all([Promise1, Promise2, Promise3, Promise4]).then((massage)=>{
+    // console.log(massage);
+    for(let i=0; i<massage.length; i++){
+        console.log(massage[i]);
     }
-})
-
-Promise1.then((massage)=>{
-    console.log("The ka massage is "+ massage);
 }).catch((error)=>{
-    console.log("Error : "+error);
-})
-
-
-// promise chaining 
-Promise1.then((massage)=>{
-    console.log("first msg : "+massage);
-    return ("Promise Fullfild ho gya Second msg");
-}).then((massage)=>{
-    console.log("Second msg : "+ massage);
-    return ("second massage fullfiled");
-}).then((massage)=>{
-    console.log("Third msg : " +massage);
-}).catch((error)=>{
-    console.log("Error: " + error);
-}).finally((massage)=>{
-    console.log("Main to final hoon : chalunga hi chalunga");
+    console.log("Error : " + error);
 })
