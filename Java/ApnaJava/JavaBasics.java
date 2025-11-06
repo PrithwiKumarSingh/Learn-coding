fbb103107de9a0bddcd77df84b5c41679451ad56
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-11-06 23:47:23 +0530

    revision of java

---
+++ Java/ApnaJava/JavaBasics.java
@@ -0,0 +1,11 @@
+public class JavaBasics{
+    public static void main(String args[]){
+        System.out.println("Hello Java !");
+        int a = 10 ;
+        int b = 20 ;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
+
+// Boilerpalet Code 
