6a4cdcf6801535dd946e86034bbe95d585132214
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-11-27 21:12:01 +0530

    Calculator dark-mode

--- files/Projects/EventLitener/script.js
+++ files/Projects/EventLitener/script.js
@@ -2,6 +2,15 @@ const main = document.querySelector(".main");
 const inp = document.querySelector(".result");
 const btn = document.querySelector(".btn");
 const clear = document.querySelector(".clear");
+const mode  = document.querySelector('#mode');
+mode.addEventListener("click",()=>{
+    document.body.classList.toggle("dark-mode");
+    if(document.body.classList.contains("dark-mode")){
+        mode.textContent = "Light";
+    }else{
+        mode.textContent = "Dark";
+    }
+})
 clear.addEventListener("click",()=>{
     inp.value = "";
 })
