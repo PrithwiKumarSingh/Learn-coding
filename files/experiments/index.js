44b351cbd1ede1edad655cececb40ffb8ae3fdc0
PrithwiKumarSingh <prithwikumar871@gmail.com>
2025-11-30 23:17:53 +0530

    experimental coding in js

---
+++ files/experiments/index.js
@@ -0,0 +1,13 @@
+const data = {
+  name : "Prithwi",
+  age : 21, 
+  address: {
+    village : "Gokhula", 
+    post : "Siswa", 
+    policStation : "Banjariya",
+    pincode: 845401
+  }
+}
+
+
+console.log(data.address.pincode);
