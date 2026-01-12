96d6ba962afb9c3ed15f865998bda7ea9cd2632e
PrithwiKumarSingh <prithwikumar871@gmail.com>
2026-01-12 14:31:42 +0530

    Strat Learning DSA Journey

---
+++ DSA/LH01/first.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a = 234;
+  float b = 2.42;
+  char c = 'A';
+  bool num = false;
+  string name = "Prithwi Kumar";
+
+  cout << name << endl;
+  cout << sizeof(b) << endl;
+  cout << name.length() << endl;
+  return 0;
+}
