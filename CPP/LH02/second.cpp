6de90b5e5b264e0b9f054d357f1e683bb530c73e
PrithwiKumarSingh <prithwikumar871@gmail.com>
2026-01-22 00:35:22 +0530

    CPP(Operators & Condition)

---
+++ CPP/LH02/second.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+int main() {
+  // int a = 'a' + 1;
+  // cout << a << endl;
+  //
+  // //char b = 'a' + 1;
+  // cout << b << endl;
+
+  // int age;
+  // cout << "Enter Your Age : ";
+  // cin >> age;
+  //
+  // if (age >= 18) {
+  //   cout << "You are eligible for Vote" << endl;
+  // } else {
+  //   cout << "You are Under 18. Not for eligible for Vote." << endl;
+  // }
+
+  // int marks;
+  // cout << "Enter Your Marks : ";
+  // cin >> marks;
+  // if (marks > 90) {
+  //   cout << "A+" << endl;
+  // } else if (marks > 80 && marks <= 90) {
+  //   cout << "A" << endl;
+  // } else if (marks > 70 && marks <= 80) {
+  //   cout << "B+" << endl;
+  // } else if (marks > 60 && marks <= 70) {
+  //   cout << "B" << endl;
+  // } else if (marks > 50 && marks <= 60) {
+  //   cout << "C+" << endl;
+  // } else if (marks > 40 && marks <= 50) {
+  //   cout << "C" << endl;
+  // } else {
+  //   cout << "Fail" << endl;
+  // }
+  //
+  // int Day;
+  // cout << "Enter Your Day : ";
+  // cin >> Day;
+  // switch (Day) {
+  // case 1:
+  //   cout << "Monday" << endl;
+  //   break;
+  // case 2:
+  //   cout << "Tuesday" << endl;
+  //   break;
+  // case 3:
+  //   cout << "Wednesday" << endl;
+  //   break;
+  // case 4:
+  //   cout << "Thursday" << endl;
+  //   break;
+  // case 5:
+  //   cout << "Friday" << endl;
+  //   break;
+  // case 6:
+  //   cout << "Saturday" << endl;
+  //   break;
+  // case 7:
+  //   cout << "Sunday" << endl;
+  // }
+  //
+  // int num;
+  // cout << "Enter Your Number : ";
+  // cin >> num;
+  // if (num % 2 == 0) {
+  //   cout << "Even" << endl;
+  // } else {
+  //   cout << "Odd" << endl;
+  // }
+  int a = 5;
+  int b = 7;
+  int c = a & b;
+  int d = a | b;
+  int e = a ^ b;
+  cout << d << endl;
+  cout << e << endl;
+
+  return 0;
+}
