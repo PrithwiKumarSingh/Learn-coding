6b38dd86c3d73e8cfa9b49457faf4fd0f9a91a38
PrithwiKumarSingh <prithwikumar871@gmail.com>
2026-04-18 15:04:20 +0530

    cnvt Array to Linkedlist

---
+++ CPP/LinkedList/SinglyLinkedList/one.cpp
@@ -0,0 +1,41 @@
+#include <iostream> 
+#include <vector>
+using namespace std; 
+
+class Node {
+    public:
+    int data; 
+    Node *next;
+
+    Node(int value){
+        data = value;
+        next = NULL;
+    }
+
+};
+
+int main(){
+
+    vector<int>arr = {2,3,4,5,6};
+
+    Node *Head;
+    Head = NULL;
+
+    for(int i=0; i<arr.size(); i++){
+        if(Head==NULL){
+        Head = new Node(arr[i]);
+         } 
+         else{
+            Node *temp;
+            temp = new Node(arr[i]);
+            temp->next = Head;
+            Head = temp;
+        }
+    }
+
+    cout<<Head->data<<endl;
+    cout<<Head->next<<endl;
+    cout<<Head<<endl;
+
+    return 0;
+}
